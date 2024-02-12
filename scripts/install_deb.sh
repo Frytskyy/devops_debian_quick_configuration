2215c918ff9d9305312b0f19cf9572cf9443c9c9
Frytskyy <78447007+Frytskyy@users.noreply.github.com>
2024-02-12 11:22:36 -0500

    Update install_deb.sh

--- scripts/install_deb.sh
+++ scripts/install_deb.sh
@@ -22,23 +22,6 @@ function update_packages
     sudo apt update
 }
 
-# Function to display the menu
-function show_menu 
-{
-    # Menu items
-    echo -e "${BLUE}${BOLD}Choose what you want to do:${NC}"  # Blue and bold text for the menu title
-    echo -e "${YELLOW}${BOLD}1)${NC} Execute ${BOLD}all steps${NC}"        # Yellow and bold text for menu items
-    echo -e "${YELLOW}${BOLD}2)${NC} Add user to ${BOLD}sudoers${NC}"
-    echo -e "${YELLOW}${BOLD}3)${NC} Install ${BOLD}system updates${NC} + ${BOLD}applications${NC} (mc, bashtop, glances, bpytop, snap, nmap, mate-system-monitor)"
-    echo -e "${YELLOW}${BOLD}4)${NC} Install ${BOLD}Wine${NC}"  # Yellow and bold text for menu items
-    echo -e "${YELLOW}${BOLD}5)${NC} Configure ${BOLD}SSH${NC} server (generate keys, set port 3444)"
-    echo -e "${YELLOW}${BOLD}6)${NC} Install ${BOLD}LAMB${NC} (Apache + MySQL + Email + PHP + domain h2.vladonai.com)"
-    echo -e "${YELLOW}${BOLD}7)${NC} Install additional ${BOLD}development${NC}/administration tools (GCC, Python, Perl, Git, QT Creator, Arduino development tools, visual GIT tools)"
-    echo -e "${YELLOW}${BOLD}8)${NC} ${BOLD}Configure security${NC} (iptables firewall, open ports for mail, 80, 443, ssh, fail2ban)"
-    echo -e "${YELLOW}${BOLD}9)${NC} Install ${BOLD}VMWare${NC} Guest Additions"
-    echo -e "${YELLOW}${BOLD}10)${NC} Exit"
-}
-
 # Function to execute all steps
 function install_step_execute_all 
 {
@@ -99,9 +82,13 @@ function install_step_install_apps
 }
 
 # Function to install Wine
-function install_step_install_wine {
-    sudo apt-get install -y wine
-    echo -e "Wine installed successfully."
+function install_step_install_wine 
+{
+    if sudo apt-get install -y wine; then
+        echo -e "${GREEN}Wine installed successfully.${NC}"
+    else
+        echo -e "${RED}Error: Wine installation failed.${NC}"
+    fi
 }
 
 # Function to configure SSH server
@@ -152,6 +139,24 @@ function install_step_install_vmware_guest_additions
 {
     sudo apt-get install -y open-vm-tools-desktop
     echo -e "VMWare Guest Additions installed successfully."
+}
+
+
+# Function to display the menu
+function show_menu 
+{
+    # Menu items
+    echo -e "${BLUE}${BOLD}Choose what you want to do:${NC}"  # Blue and bold text for the menu title
+    echo -e "${YELLOW}${BOLD}1)${NC} Execute ${BOLD}all steps${NC}"        # Yellow and bold text for menu items
+    echo -e "${YELLOW}${BOLD}2)${NC} Add user to ${BOLD}sudoers${NC}"
+    echo -e "${YELLOW}${BOLD}3)${NC} Install ${BOLD}system updates${NC} + ${BOLD}applications${NC} (mc, bashtop, glances, bpytop, snap, nmap, mate-system-monitor)"
+    echo -e "${YELLOW}${BOLD}4)${NC} Install ${BOLD}Wine${NC}"  # Yellow and bold text for menu items
+    echo -e "${YELLOW}${BOLD}5)${NC} Configure ${BOLD}SSH${NC} server (generate keys, set port 3444)"
+    echo -e "${YELLOW}${BOLD}6)${NC} Install ${BOLD}LAMB${NC} (Apache + MySQL + Email + PHP + domain h2.vladonai.com)"
+    echo -e "${YELLOW}${BOLD}7)${NC} Install additional ${BOLD}development${NC}/administration tools (GCC, Python, Perl, Git, QT Creator, Arduino development tools, visual GIT tools)"
+    echo -e "${YELLOW}${BOLD}8)${NC} ${BOLD}Configure security${NC} (iptables firewall, open ports for mail, 80, 443, ssh, fail2ban)"
+    echo -e "${YELLOW}${BOLD}9)${NC} Install ${BOLD}VMWare${NC} Guest Additions"
+    echo -e "${YELLOW}${BOLD}q)${NC} Exit"
 }
 
 # Main code
